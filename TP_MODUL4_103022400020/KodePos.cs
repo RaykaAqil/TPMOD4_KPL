6ce5164c432a04732dfb35a5cd408ef8893fea0d
RaykaAqil <raykaaqil19@gmail.com>
2026-03-16 00:06:14 +0700

    second commit

---
+++ TP_MODUL4_103022400020/KodePos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL4_103022400020
+{
+    public class KodePos
+    {
+        public enum Kelurahan
+        {
+            Batununggal, Kujangsari, Mengger, Wates, Cijaura,
+            Jatisari, Margasari, Sekejati, Kebonwaru, Maleer
+        }
+
+        public static int GetKodePos(Kelurahan kel)
+        {
+            int[] tabelKodePos = {
+            40266, 40287, 40267, 40256, 40287,
+            40286, 40286, 40286, 40272, 40274
+            };
+
+            return tabelKodePos[(int)kel];
+        }
+    }
+}
--- TP_MODUL4_103022400020/Program.cs
+++ TP_MODUL4_103022400020/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TP_MODUL4_103022400020;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos.Kelurahan kelurahan1 = KodePos.Kelurahan.Batununggal;
+        int kodePosHasil1 = KodePos.GetKodePos(kelurahan1);
+
+        Console.WriteLine("Nama Kelurahan : " + kelurahan1.ToString());
+        Console.WriteLine("Kode Pos : " + kodePosHasil1);
+
+        KodePos.Kelurahan kelurahan2 = KodePos.Kelurahan.Kujangsari;
+        int kodePosHasil2 = KodePos.GetKodePos(kelurahan2);
+
+        Console.WriteLine("Nama Kelurahan : " + kelurahan2.ToString());
+        Console.WriteLine("Kode Pos : " + kodePosHasil2);
+    }
+}
